0a7766164f0a9c0095b101c43e969cef157f17f4
robby <robby.remmenga@gmail.com>
2019-11-15 15:00:03 -0500

    initial commit

---
+++ src/main/java/recyclemadeeasy/recyclemadeeasy/RecycleMadeEasyApplication.java
@@ -0,0 +1,13 @@
+package recyclemadeeasy.recyclemadeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleMadeEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleMadeEasyApplication.class, args);
+	}
+
+}
